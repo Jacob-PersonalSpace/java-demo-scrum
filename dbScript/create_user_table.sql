9f48ec5e030d3075af13b74ba8b054076eb4927a
GFG1\fengjac <fengjac@esquel.com>
2018-12-06 13:17:33 +0800

    <fea> migrate with mysql in docker compose

---
+++ dbScript/create_user_table.sql
@@ -0,0 +1,16 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
