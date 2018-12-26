524ec56c5fc50080f626d06d7e434ecb89580617
GFG1\fengjac <fengjac@esquel.com>
2018-12-26 21:12:43 +0800

    <fea> generate token in unit test

--- src/main/java/com/demo/scrum/DemoApplication.java
+++ src/main/java/com/demo/scrum/DemoApplication.java
@@ -2,12 +2,10 @@ package com.demo.scrum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
-@EnableJpaAuditing
 public class DemoApplication {
 
     public static void main(String[] args) {
